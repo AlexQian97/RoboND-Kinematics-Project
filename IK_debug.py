95acffd98e8bb8e66cc66f35c6b6fcf1a4c92e7c
Wenzhang Qian <wenzhangq@gmail.com>
2017-09-24 13:53:10 -0700

    debug failed

--- IK_debug.py
+++ IK_debug.py
@@ -10,28 +10,29 @@ From here you can adjust the joint angles to find thetas, use the gripper to ext
 to find the position of the wrist center. These newly generated test cases can be added to the test_cases dictionary.
 '''
 
-test_cases = {1:[[[2.16135,-1.42635,1.55109],
-                  [0.708611,0.186356,-0.157931,0.661967]],
-                  [1.89451,-1.44302,1.69366],
-                  [-0.65,0.45,-0.36,0.95,0.79,0.49]],
-              2:[[[-0.56754,0.93663,3.0038],
-                  [0.62073, 0.48318,0.38759,0.480629]],
-                  [-0.638,0.64198,2.9988],
-                  [-0.79,-0.11,-2.33,1.94,1.14,-3.68]],
-              3:[[[-1.3863,0.02074,0.90986],
-                  [0.01735,-0.2179,0.9025,0.371016]],
-                  [-1.1669,-0.17989,0.85137],
-                  [-2.99,-0.12,0.94,4.06,1.29,-4.12]],
-              4:[],
-              5:[]}
+test_cases = {1: [[[2.16135, -1.42635, 1.55109],
+                   [0.708611, 0.186356, -0.157931, 0.661967]],
+                  [1.89451, -1.44302, 1.69366],
+                  [-0.65, 0.45, -0.36, 0.95, 0.79, 0.49]],
+              2: [[[-0.56754, 0.93663, 3.0038],
+                   [0.62073, 0.48318, 0.38759, 0.480629]],
+                  [-0.638, 0.64198, 2.9988],
+                  [-0.79, -0.11, -2.33, 1.94, 1.14, -3.68]],
+              3: [[[-1.3863, 0.02074, 0.90986],
+                   [0.01735, -0.2179, 0.9025, 0.371016]],
+                  [-1.1669, -0.17989, 0.85137],
+                  [-2.99, -0.12, 0.94, 4.06, 1.29, -4.12]],
+              4: [],
+              5: []}
 
 
 def createMatrix(alpha, a, q, d):
-    mat =  Matrix([[            cos(q),           -sin(q),           0,             a],
-                   [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-                   [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
-                   [                 0,                 0,           0,             1]])
+    mat = Matrix([[cos(q), -sin(q), 0, a],
+                  [sin(q) * cos(alpha), cos(q) * cos(alpha), -sin(alpha), -sin(alpha) * d],
+                  [sin(q) * sin(alpha), cos(q) * sin(alpha), cos(alpha), cos(alpha) * d],
+                  [0, 0, 0, 1]])
     return mat
+
 
 def radains(deg):
     return deg * pi / 180
@@ -41,13 +42,15 @@ def test_code(test_case):
     ## Set up code
     ## Do not modify!
     x = 0
+
     class Position:
-        def __init__(self,EE_pos):
+        def __init__(self, EE_pos):
             self.x = EE_pos[0]
             self.y = EE_pos[1]
             self.z = EE_pos[2]
+
     class Orientation:
-        def __init__(self,EE_ori):
+        def __init__(self, EE_ori):
             self.x = EE_ori[0]
             self.y = EE_ori[1]
             self.z = EE_ori[2]
@@ -57,24 +60,24 @@ def test_code(test_case):
     orientation = Orientation(test_case[0][1])
 
     class Combine:
-        def __init__(self,position,orientation):
+        def __init__(self, position, orientation):
             self.position = position
             self.orientation = orientation
 
-    comb = Combine(position,orientation)
+    comb = Combine(position, orientation)
 
     class Pose:
-        def __init__(self,comb):
+        def __init__(self, comb):
             self.poses = [comb]
 
     req = Pose(comb)
     start_time = time()
-    
+
     ########################################################################################
-    ## 
+	# Insert IK code here!
+	#  Create symbols
 
-    ## Insert IK code here!
-
+	# d0, d1, d2, d3, d4, d5, d6 = symbols('d0:7')
     d0, d1, d2, d3, d4, d5, d6 = symbols('d0:7')
     a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
     alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
@@ -82,13 +85,12 @@ def test_code(test_case):
 
     # Create Modified DH parameters
     dh_params = {alpha0: 0, a0: 0, d0: 0.75, q0: q0,
-                 alpha1: -pi / 2, a1: 0.35, d1: 0, q1: -pi / 2 + q1,
-                 alpha2: 0, a2: 1.25, d2: 0, q2: q2,
-                 alpha3: -pi / 2, a3: -0.054, d3: 1.50, q3: q3,
-                 alpha4: pi / 2, a4: 0, d4: 1.50, q4: q4,
-                 alpha5: -pi / 2, a5: 0, d5: 0, q5: q5,
-                 alpha6: 0, a6: 0, d6: 0.303, q6: 0}
-
+				 alpha1: -pi / 2, a1: 0.35, d1: 0, q1: -pi / 2 + q1,
+				 alpha2: 0, a2: 1.25, d2: 0, q2: q2,
+				 alpha3: -pi / 2, a3: -0.054, d3: 1.50, q3: q3,
+				 alpha4: pi / 2, a4: 0, d4: 1.50, q4: q4,
+				 alpha5: -pi / 2, a5: 0, d5: 0, q5: q5,
+				 alpha6: 0, a6: 0, d6: 0.303, q6: 0}
     # Define Modified DH Transformation matrix
     T0_1 = createMatrix(alpha0, a0, q0, d0).subs(dh_params)
     T1_2 = createMatrix(alpha1, a1, q1, d1).subs(dh_params)
@@ -105,20 +107,35 @@ def test_code(test_case):
     T0_5 = T0_4 * T4_5
     T0_6 = T0_5 * T5_6
     T0_G = T0_6 * T6_G
-    T0_G = simplify(T0_6)
 
     # Extract rotation matrices from the transformation matrices
-    R0_1 = T0_1[0:3, 0:3]
-    R0_2 = T0_2[0:3, 0:3]
+	#  R0_1 = T0_1[0:3, 0:3]
+    # R0_2 = T0_2[0:3, 0:3]
     R0_3 = T0_3[0:3, 0:3]
-    R0_4 = T0_4[0:3, 0:3]
-    R0_5 = T0_5[0:3, 0:3]
-    R0_6 = T0_6[0:3, 0:3]
-    R0_G = T0_G[0:3, 0:3]
-    # print(T0_1)
-    # print(simplify(T0_G))
-    # print("1 reached")
-    # IK code starts here
+    # R0_4 = T0_4[0:3, 0:3]
+    # R0_5 = T0_5[0:3, 0:3]
+    # R0_6 = T0_6[0:3, 0:3]
+    # R0_G = T0_G[0:3, 0:3]
+
+    r, p, y = symbols("r p y")
+
+    ROT_x = Matrix([[1, 0, 0],
+					[0, cos(r), -sin(r)],
+					[0, sin(r), cos(r)]])
+
+    ROT_y = Matrix([[cos(p), 0, sin(p)],
+					[0, 1, 0],
+					[-sin(p), 0, cos(p)]])
+
+    ROT_z = Matrix([[cos(y), -sin(y), 0],
+					[sin(y), cos(y), 0],
+					[0, 0, 1]])
+
+    ROT_EE = ROT_z * ROT_y * ROT_x
+    ROT_Error = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
+
+    ROT_EE = ROT_EE * ROT_Error
+	# IK code starts here
 
     # Extract end-effector position and orientation from request
     # px,py,pz = end-effector position
@@ -128,36 +145,17 @@ def test_code(test_case):
     pz = req.poses[x].position.z
 
     (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
-        [req.poses[x].orientation.x, req.poses[x].orientation.y,
-         req.poses[x].orientation.z, req.poses[x].orientation.w])
+		[req.poses[x].orientation.x, req.poses[x].orientation.y,
+		 req.poses[x].orientation.z, req.poses[x].orientation.w])
 
     ### Your IK code here
     # Compensate for rotation discrepancy between DH parameters and Gazebo
 
-    r, p, y = symbols("r p y")
-
-    ROT_x = Matrix([[1, 0, 0],
-                    [0, cos(r), -sin(r)],
-                    [0, sin(r), cos(r)]])
-
-    ROT_y = Matrix([[cos(p), 0, sin(p)],
-                    [0, 1, 0],
-                    [-sin(p), 0, cos(p)]])
-
-    ROT_z = Matrix([[cos(y), -sin(y), 0],
-                    [sin(y), cos(y), 0],
-                    [0, 0, 1]])
-
-    ROT_EE = ROT_z * ROT_y * ROT_x
-
-    ROT_Error = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
-
-    ROT_EE = ROT_EE * ROT_Error
-    ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
+    ROT_EE = ROT_EE.subs({"r": roll, "p": pitch, "y": yaw})
 
     EE = Matrix([[px],
-                 [py],
-                 [pz]])
+				 [py],
+				 [pz]])
 
     WC = EE - 0.303 * ROT_EE[:, 2]
 
@@ -173,19 +171,13 @@ def test_code(test_case):
     angle_a = acos((side_b ** 2 + side_c ** 2 - side_a ** 2) / (2 * side_b * side_c))
     angle_b = acos((side_a ** 2 + side_c ** 2 - side_b ** 2) / (2 * side_a * side_c))
     # angle_c = acos((side_b ** 2 + side_b ** 2 - side_c ** 2) / (2 * side_a * side_b))
+
     theta2 = pi / 2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
     theta3 = pi / 2 - (angle_b + 0.036)
-    # (-0.872902929429671, -1.74379649447214 + pi / 2, -0.59010686452587 + pi / 2)
 
-    # R0_3 = T0_3
     R0_3 = R0_3.evalf(subs={q0: theta1, q1: theta2, q2: theta3})
 
-    # print("!!!!!!!!!!!")
-    # print(R0_3)
-    # print(R0_3.inv("LU"))
-    # print(ROT_EE)
     R3_6 = R0_3.inv("LU") * ROT_EE
-    # print("??????")
 
     theta4 = atan2(R3_6[2, 2], -R3_6[0, 2])
     theta5 = atan2(sqrt(R3_6[0, 2] * R3_6[0, 2] + R3_6[2, 2] * R3_6[2, 2]), R3_6[1, 2])
@@ -204,31 +196,31 @@ def test_code(test_case):
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
-    your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
+    your_wc = [1, 1, 1]  # <--- Load your calculated WC values in this array
+    your_ee = [1, 1, 1]  # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
 
     ## Error analysis
-    print ("\nTotal run time to calculate joint angles from pose is %04.4f seconds" % (time()-start_time))
+    print ("\nTotal run time to calculate joint angles from pose is %04.4f seconds" % (time() - start_time))
 
     # Find WC error
-    if not(sum(your_wc)==3):
-        wc_x_e = abs(your_wc[0]-test_case[1][0])
-        wc_y_e = abs(your_wc[1]-test_case[1][1])
-        wc_z_e = abs(your_wc[2]-test_case[1][2])
-        wc_offset = sqrt(wc_x_e**2 + wc_y_e**2 + wc_z_e**2)
+    if not (sum(your_wc) == 3):
+        wc_x_e = abs(your_wc[0] - test_case[1][0])
+        wc_y_e = abs(your_wc[1] - test_case[1][1])
+        wc_z_e = abs(your_wc[2] - test_case[1][2])
+        wc_offset = sqrt(wc_x_e ** 2 + wc_y_e ** 2 + wc_z_e ** 2)
         print ("\nWrist error for x position is: %04.8f" % wc_x_e)
         print ("Wrist error for y position is: %04.8f" % wc_y_e)
         print ("Wrist error for z position is: %04.8f" % wc_z_e)
         print ("Overall wrist offset is: %04.8f units" % wc_offset)
 
     # Find theta errors
-    t_1_e = abs(theta1-test_case[2][0])
-    t_2_e = abs(theta2-test_case[2][1])
-    t_3_e = abs(theta3-test_case[2][2])
-    t_4_e = abs(theta4-test_case[2][3])
-    t_5_e = abs(theta5-test_case[2][4])
-    t_6_e = abs(theta6-test_case[2][5])
+    t_1_e = abs(theta1 - test_case[2][0])
+    t_2_e = abs(theta2 - test_case[2][1])
+    t_3_e = abs(theta3 - test_case[2][2])
+    t_4_e = abs(theta4 - test_case[2][3])
+    t_5_e = abs(theta5 - test_case[2][4])
+    t_6_e = abs(theta6 - test_case[2][5])
     print ("\nTheta 1 error is: %04.8f" % t_1_e)
     print ("Theta 2 error is: %04.8f" % t_2_e)
     print ("Theta 3 error is: %04.8f" % t_3_e)
@@ -241,17 +233,15 @@ def test_code(test_case):
     print (" ")
 
     # Find FK EE error
-    if not(sum(your_ee)==3):
-        ee_x_e = abs(your_ee[0]-test_case[0][0][0])
-        ee_y_e = abs(your_ee[1]-test_case[0][0][1])
-        ee_z_e = abs(your_ee[2]-test_case[0][0][2])
-        ee_offset = sqrt(ee_x_e**2 + ee_y_e**2 + ee_z_e**2)
+    if not (sum(your_ee) == 3):
+        ee_x_e = abs(your_ee[0] - test_case[0][0][0])
+        ee_y_e = abs(your_ee[1] - test_case[0][0][1])
+        ee_z_e = abs(your_ee[2] - test_case[0][0][2])
+        ee_offset = sqrt(ee_x_e ** 2 + ee_y_e ** 2 + ee_z_e ** 2)
         print ("\nEnd effector error for x position is: %04.8f" % ee_x_e)
         print ("End effector error for y position is: %04.8f" % ee_y_e)
         print ("End effector error for z position is: %04.8f" % ee_z_e)
         print ("Overall end effector offset is: %04.8f units \n" % ee_offset)
-
-
 
 
 if __name__ == "__main__":
--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -17,11 +17,13 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Pose
 from mpmath import *
 from sympy import *
+
+
 def createMatrix(alpha, a, q, d):
-    mat =  Matrix([[            cos(q),           -sin(q),           0,             a],
-                   [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-                   [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
-                   [                 0,                 0,           0,             1]])
+    mat = Matrix([[cos(q), -sin(q), 0, a],
+                  [sin(q) * cos(alpha), cos(q) * cos(alpha), -sin(alpha), -sin(alpha) * d],
+                  [sin(q) * sin(alpha), cos(q) * sin(alpha), cos(alpha), cos(alpha) * d],
+                  [0, 0, 0, 1]])
 
     return mat
 
@@ -32,7 +34,7 @@ def handle_calculate_IK(req):
         print "No valid poses received"
         return -1
     else:
-		
+
         ### Your FK code here
         # Create symbols
         d0, d1, d2, d3, d4, d5, d6 = symbols('d0:7')
@@ -41,22 +43,22 @@ def handle_calculate_IK(req):
         q0, q1, q2, q3, q4, q5, q6 = symbols('q0:7')
 
         # Create Modified DH parameters
-        dh_params = {alpha0:     0, a0:     0, d0:   0.75, q0: q0,
-                     alpha1: -pi/2, a1:  0.35, d1:      0, q1: -pi/2 + q1,
-                     alpha2:     0, a2:  1.25, d2:      0, q2: q2,
-                     alpha3: -pi/2, a3:-0.054, d3:   1.50, q3: q3,
-                     alpha4:  pi/2, a4:     0, d4:   1.50, q4: q4,
-                     alpha5: -pi/2, a5:     0, d5:      0, q5: q5,
-                     alpha6:     0, a6:     0, d6:  0.303, q6: 0}
+        dh_params = {alpha0: 0, a0: 0, d0: 0.75, q0: q0,
+                     alpha1: -pi / 2, a1: 0.35, d1: 0, q1: -pi / 2 + q1,
+                     alpha2: 0, a2: 1.25, d2: 0, q2: q2,
+                     alpha3: -pi / 2, a3: -0.054, d3: 1.50, q3: q3,
+                     alpha4: pi / 2, a4: 0, d4: 1.50, q4: q4,
+                     alpha5: -pi / 2, a5: 0, d5: 0, q5: q5,
+                     alpha6: 0, a6: 0, d6: 0.303, q6: 0}
 
         # Define Modified DH Transformation matrix
-        T0_1 = createMatrix(alpha0, a0, q0, d0)
-        T1_2 = createMatrix(alpha1, a1, q1, d1)
-        T2_3 = createMatrix(alpha2, a2, q2, d2)
-        T3_4 = createMatrix(alpha3, a3, q3, d3)
-        T4_5 = createMatrix(alpha4, a4, q4, d4)
-        T5_6 = createMatrix(alpha5, a5, q5, d5)
-        T6_G = createMatrix(alpha6, a6, q6, d6)
+        T0_1 = createMatrix(alpha0, a0, q0, d0).subs(dh_params)
+        T1_2 = createMatrix(alpha1, a1, q1, d1).subs(dh_params)
+        T2_3 = createMatrix(alpha2, a2, q2, d2).subs(dh_params)
+        T3_4 = createMatrix(alpha3, a3, q3, d3).subs(dh_params)
+        T4_5 = createMatrix(alpha4, a4, q4, d4).subs(dh_params)
+        T5_6 = createMatrix(alpha5, a5, q5, d5).subs(dh_params)
+        T6_G = createMatrix(alpha6, a6, q6, d6).subs(dh_params)
 
         # Create individual transformation matrices
         T0_2 = T0_1 * T1_2
@@ -67,19 +69,39 @@ def handle_calculate_IK(req):
         T0_G = T0_6 * T6_G
 
         # Extract rotation matrices from the transformation matrices
-        R0_1 = T0_1[0:3, 0:3]
-        R0_2 = T0_2[0:3, 0:3]
+        # R0_1 = T0_1[0:3, 0:3]
+        # R0_2 = T0_2[0:3, 0:3]
         R0_3 = T0_3[0:3, 0:3]
-        R0_4 = T0_4[0:3, 0:3]
-        R0_5 = T0_5[0:3, 0:3]
-        R0_6 = T0_6[0:3, 0:3]
-        R0_G = T0_G[0:3, 0:3]
+        # R0_4 = T0_4[0:3, 0:3]
+        # R0_5 = T0_5[0:3, 0:3]
+        # R0_6 = T0_6[0:3, 0:3]
+        # R0_G = T0_G[0:3, 0:3]
 
-        # Initialize service response
+        r, p, y = symbols("r p y")
+
+        ROT_x = Matrix([[1, 0, 0],
+						[0, cos(r), -sin(r)],
+						[0, sin(r), cos(r)]])
+
+        ROT_y = Matrix([[cos(p), 0, sin(p)],
+						[0, 1, 0],
+						[-sin(p), 0, cos(p)]])
+
+        ROT_z = Matrix([[cos(y), -sin(y), 0],
+						[sin(y), cos(y), 0],
+						[0, 0, 1]])
+
+        ROT_EE = ROT_z * ROT_y * ROT_x
+        ROT_Error = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
+
+        ROT_EE = ROT_EE * ROT_Error
+
+		# Initialize service response
         joint_trajectory_list = []
         for x in xrange(0, len(req.poses)):
             # IK code starts here
             joint_trajectory_point = JointTrajectoryPoint()
+            print('Calculating %s from %s' % (x + 1, len(req.poses)))
 
             # Extract end-effector position and orientation from request
             # px,py,pz = end-effector position
@@ -90,30 +112,11 @@ def handle_calculate_IK(req):
 
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
-                    req.poses[x].orientation.z, req.poses[x].orientation.w])
+                 req.poses[x].orientation.z, req.poses[x].orientation.w])
 
             ### Your IK code here
-	        # Compensate for rotation discrepancy between DH parameters and Gazebo
+            # Compensate for rotation discrepancy between DH parameters and Gazebo
 
-            r, p, y = symbols("r p y")
-
-            ROT_x = Matrix([[1, 0, 0],
-                            [0, cos(r), -sin(r)],
-                            [0, sin(r), cos(r)]])
-
-            ROT_y = Matrix([[cos(p), 0, sin(p)],
-                            [0, 1, 0],
-                            [-sin(p), 0, cos(p)]])
-
-            ROT_z = Matrix([[cos(y), -sin(y), 0],
-                            [sin(y), cos(y), 0],
-                            [0, 0, 1]])
-
-            ROT_EE = ROT_z * ROT_y * ROT_x
-
-            ROT_Error = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
-
-            ROT_EE = ROT_EE * ROT_Error
             ROT_EE = ROT_EE.subs({"r": roll, "p": pitch, "y": yaw})
 
             EE = Matrix([[px],
@@ -143,8 +146,8 @@ def handle_calculate_IK(req):
             R3_6 = R0_3.inv("LU") * ROT_EE
 
             theta4 = atan2(R3_6[2, 2], -R3_6[0, 2])
-            theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[2,2] * R3_6[2,2]), R3_6[1,2])
-            theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+            theta5 = atan2(sqrt(R3_6[0, 2] * R3_6[0, 2] + R3_6[2, 2] * R3_6[2, 2]), R3_6[1, 2])
+            theta6 = atan2(-R3_6[1, 1], R3_6[1, 0])
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
@@ -162,6 +165,7 @@ def IK_server():
     s = rospy.Service('calculate_ik', CalculateIK, handle_calculate_IK)
     print "Ready to receive an IK request"
     rospy.spin()
+
 
 if __name__ == "__main__":
     IK_server()
